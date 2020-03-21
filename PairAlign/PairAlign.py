24384e886c0154ed290f6ebefc2de43826003eba
hbashanae <hbashanae@gmail.com>
2020-03-21 19:11:08 +0530

    waterman

--- PairAlign/PairAlign.py
+++ PairAlign/PairAlign.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,jsonify,request
 from PairAlign.Algorithms.NW import NW
+from PairAlign.Algorithms.SW import SW
 
 pair_align_bp = Blueprint('pair_align_bp', __name__)
 
@@ -10,5 +11,15 @@ def pair_nw():
     mismatch = int(request_data['mismatch'])
     gap = int(request_data['gap'])
     results = NW(request_data['seq_a'],request_data['seq_b'],match,mismatch,gap)
+    
+    return jsonify({'results':results})
+
+@pair_align_bp.route('/sw',methods=['POST'])
+def pair_sw():
+    request_data = request.get_json()
+    match = int(request_data['match'])
+    mismatch = int(request_data['mismatch'])
+    gap = int(request_data['gap'])
+    results = SW(request_data['seq_a'],request_data['seq_b'],match,mismatch,gap)
     
     return jsonify({'results':results})
