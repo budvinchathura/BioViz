a23c2edec077d2d4d5d813bd87fda84644c8d9cd
hbashanae <hbashanae@gmail.com>
2020-03-28 23:34:36 +0530

    progressive optimal minimal

--- MSA/Algorithms/ProgressiveOptimal.py
+++ MSA/Algorithms/ProgressiveOptimal.py
@@ -1,6 +1,59 @@
+import numpy as np
 from MSA.Algorithms.Algorithm import Algorithm
+from PairAlign.Algorithms.NW import NW
+from PairAlign.Executer import Executer
+from MSA.Algorithms.NWProf import NWProf
+
 
 class ProgressiveOptimal(Algorithm):
-    
-    def initialize(self):
-        pass
+
+    def __init__(self, sequences, match_score=1, mismatch_penalty=-1, gap_penalty=-1):
+        self.sequences = sequences
+        self.match_score = match_score
+        self.mismatch_penalty = mismatch_penalty
+        self.gap_penalty = gap_penalty
+        self.alignments = []
+        self.intermediate_profs = []
+        self.identity = 0
+
+    def align(self):
+        l = len(self.sequences)
+        while l > 1:
+            similarity_matrix = np.zeros((l, l))
+            for i in range(l):
+                for j in range(i, l):
+                    if i == j:
+                        continue
+                    nw_prof_algorithm = NWProf(self.sequences[i], self.sequences[j], self.match_score, self.mismatch_penalty, self.gap_penalty)
+                    executer = Executer(nw_prof_algorithm)
+                    result = executer.get_results()
+                    similarity = result['alignments'][0]['identity']
+                    similarity_matrix[i][j] = similarity
+            seq1i, seq2i = np.where(
+                similarity_matrix == np.amax(similarity_matrix))
+            seq1i, seq2i = seq1i[0], seq2i[0]
+            if seq1i > seq2i:
+                a = self.sequences.pop(seq1i)
+                b = self.sequences.pop(seq2i)
+                seq1 = a if isinstance(a, list) else [a]
+                seq2 = b if isinstance(b, list) else [b]
+            else:
+                b = self.sequences.pop(seq2i)
+                a = self.sequences.pop(seq1i)
+                seq2 = b if isinstance(b, list) else [b]
+                seq1 = a if isinstance(a, list) else [a]
+            nw_prof_algorithm = NWProf(seq1, seq2, self.match_score, self.mismatch_penalty, self.gap_penalty)
+            executer = Executer(nw_prof_algorithm)
+            result = executer.get_results()
+            align_a = result['alignments'][0]['algn_a']
+            align_b = result['alignments'][0]['algn_b']
+            prof = align_a + align_b
+            print(prof)
+            self.intermediate_profs.append(prof)
+            self.sequences.append(prof)
+            l = len(self.sequences)
+        self.alignments = prof
+        self.identity = result['alignments'][0]['identity']
+
+    def get_alignments(self) -> list:
+        return {'alignments': self.alignments, 'intermediate' :self.intermediate_profs, 'identity': self.identity}
--- MSA/MSA.py
+++ MSA/MSA.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 
 from MSA.Executer import Executer
 from MSA.Algorithms.Progressive import Progressive
+from MSA.Algorithms.ProgressiveOptimal import ProgressiveOptimal
 
 msa_bp = Blueprint('msa_bp', __name__)
 
@@ -9,7 +10,6 @@ msa_bp = Blueprint('msa_bp', __name__)
 @msa_bp.route('/progressive', methods=['POST'])
 def progressive():
     request_data = request.get_json()
-    # print(request_data)
     match = int(request_data['match'])
     mismatch = int(request_data['mismatch'])
     gap = int(request_data['gap'])
@@ -25,4 +25,15 @@ def progressive():
 
 @msa_bp.route('/progressive-optimal', methods=['POST'])
 def progressive_optimal():
-    return jsonify({})
+    request_data = request.get_json()
+    match = int(request_data['match'])
+    mismatch = int(request_data['mismatch'])
+    gap = int(request_data['gap'])
+    progressive_algorithm = ProgressiveOptimal(
+        request_data['sequences'], match, mismatch, gap)
+
+    executer = Executer(progressive_algorithm)
+    result = executer.get_results()
+
+    resp = {'result': result}
+    return jsonify(resp)
