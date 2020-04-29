9bce81c407c31d1db42daf31edecf29447e88fe5
Budvin Chathura <chathura97rc@gmail.com>
2020-04-30 00:41:30 +0530

    structure added for pairalign extended algorithms

---
+++ PairAlign/Algorithms/NW_extended.py
@@ -0,0 +1,4 @@
+from PairAlign.Algorithms.Algorithm import Algorithm
+
+class NWExtended(Algorithm):
+    pass
---
+++ PairAlign/Algorithms/SW_extended.py
@@ -0,0 +1,4 @@
+from PairAlign.Algorithms.Algorithm import Algorithm
+
+class SWExtended(Algorithm):
+    pass
